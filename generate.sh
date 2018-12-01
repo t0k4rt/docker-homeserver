00cca20d36b8f5d041b426702c058a2d035ac202
Alexandre Assouad <assouad@kairosfire.com>
2018-12-01 18:16:38 +0100

    migrated to traefik

--- generate.sh
+++ generate.sh
@@ -7,17 +7,32 @@ NEXTCLOUD_PASSWORD=""
 NEXTCLOUD_DB="nextcloud"
 NEXTCLOUD_DB_USER="nextcloud"
 NEXTCLOUD_DATA=""
+NEXTCLOUD_CONFIG=""
 NEXTCLOUD_VHOST="nextcloud.toktok.fr"
-NEXTCLOUD_LETSENCRYPT_HOST="nextcloud.toktok.fr"
 
 COCKPIT_VHOST="cockpit.toktok.fr"
-COCKPIT_LETSENCRYPT_HOST="cockpit.toktok.fr"
 
 TRANSMISSION_VHOST="transmission.toktok.fr"
-TRANSMISSION_LETSENCRYPT_HOST="transmission.toktok.fr"
-TRANSMISSION_VIRTUAL_PORT="9091"
 TRANSMISSION_DATA=""
 TRANSMISSION_CONFIG=""
+
+TRAEFIK_CONF_DIR=""
+TRAEFIK_LETSENCRYPT_DIR=""
+TRAEFIK_CONF_FILE="${TRAEFIK_CONF_DIR}/traefik.toml"
+TRAEFIK_LETSENCRYPT_FILE="${TRAEFIK_CONF_DIR}/acme.conf"
+
+
+if [ -e $TRAEFIK_CONF_FILE ]; then 
+    touch $TRAEFIK_CONF_FILE
+fi
+
+if [ -e $TRAEFIK_LETSENCRYPT_FILE ]; then 
+    touch $TRAEFIK_LETSENCRYPT_FILE
+    chmod 600 $TRAEFIK_LETSENCRYPT_FILE
+fi
+
+DOMAIN="toktok.fr"
+VHOST_LIST="\"${TRANSMISSION_VHOST}\",\"${NEXTCLOUD_VHOST}\",\"${COCKPIT_VHOST}\""
 
 SUBJECT="Let's encrypt email"
 while true; do
@@ -28,6 +43,19 @@ while true; do
         break
     fi
 done
+
+
+SUBJECT="Traefik config dir"
+while true; do
+    read -p "$SUBJECT: " TRAEFIK_CONF_DIR
+    if [ -z "$TRAEFIK_CONF_DIR" ]; then
+        echo "$SUBJECT cannot be empty, try again"
+    else
+        TRAEFIK_LETSENCRYPT_DIR="$TRAEFIK_CONF_DIR"
+        break
+    fi
+done
+
 
 SUBJECT="Nextcloud password"
 while true; do
@@ -64,15 +92,14 @@ while true; do
 done
 
 
-SUBJECT="Nextcloud vhost / letsencrypt host"
+SUBJECT="Nextcloud vhost"
 while true; do
     read -p "$SUBJECT: " value
     if [ -z "$value" ]; then
-        echo "Using default $SUBJECT: $NEXTCLOUD_VHOST / $NEXTCLOUD_LETSENCRYPT_HOST"
+        echo "Using default $SUBJECT: $NEXTCLOUD_VHOST"
         break
     else
         NEXTCLOUD_VHOST="$value"
-        NEXTCLOUD_LETSENCRYPT_HOST="$value"
         break
     fi
 done
@@ -88,46 +115,42 @@ while true; do
     fi
 done
 
+SUBJECT="Nextcloud config dir"
+while true; do
+    read -p "$SUBJECT: " NEXTCLOUD_CONFIG
+    if [ -z "$NEXTCLOUD_CONFIG" ]; then
+        echo "$SUBJECT cannot be empty, try again"
+    else
+        break
+    fi
+done
 
-SUBJECT="Cockpit vhost / letsencrypt host"
+
+SUBJECT="Cockpit vhost"
 while true; do
     read -p "$SUBJECT: " value
     if [ -z "$value" ]; then
-        echo "Using default $SUBJECT: $COCKPIT_VHOST / $COCKPIT_LETSENCRYPT_HOST"
+        echo "Using default $SUBJECT: $COCKPIT_VHOST"
         break
     else
         COCKPIT_VHOST="$value"
-        COCKPIT_LETSENCRYPT_HOST="$value"
         break
     fi
 done
 
 
-SUBJECT="Transmission vhost / letsencrypt host"
+SUBJECT="Transmission vhost"
 while true; do
     read -p "$SUBJECT: " value
     if [ -z "$value" ]; then
-        echo "Using default $SUBJECT: $TRANSMISSION_VHOST / $TRANSMISSION_LETSENCRYPT_HOST"
+        echo "Using default $SUBJECT: $TRANSMISSION_VHOST"
         break
     else
         TRANSMISSION_VHOST="$value"
-        TRANSMISSION_LETSENCRYPT_HOST="$value"
         break
     fi
 done
 
-
-SUBJECT="Transmission virtual port"
-while true; do
-    read -p "$SUBJECT: " value
-    if [ -z "$value" ]; then
-        echo "Using default $SUBJECT: $TRANSMISSION_VIRTUAL_PORT"
-        break
-    else
-        TRANSMISSION_VIRTUAL_PORT="$value"
-        break
-    fi
-done
 
 SUBJECT="Transmission data dir"
 while true; do
@@ -151,7 +174,8 @@ while true; do
 done
 
 cat db.tpl.env | sed -e "s|{{NEXTCLOUD_PASSWORD}}|$NEXTCLOUD_PASSWORD|g;s|{{NEXTCLOUD_DB}}|$NEXTCLOUD_DB|g;s|{{NEXTCLOUD_DB_USER}}|$NEXTCLOUD_DB_USER|g" > db.env
-cat web_cockpit.tpl.env | sed -e "s|{{LETSENCRYPT_MAIL}}|$LETSENCRYPT_MAIL|g;s|{{COCKPIT_VHOST}}|$COCKPIT_VHOST|g;s|{{COCKPIT_LETSENCRYPT_HOST}}|$COCKPIT_LETSENCRYPT_HOST|g" > web_cockpit.env
-cat web_nextcloud.tpl.env | sed -e "s|{{LETSENCRYPT_MAIL}}|$LETSENCRYPT_MAIL|g;s|{{NEXTCLOUD_VHOST}}|$NEXTCLOUD_VHOST|g;s|{{NEXTCLOUD_LETSENCRYPT_HOST}}|$NEXTCLOUD_LETSENCRYPT_HOST|g" > web_nextcloud.env
-cat transmission.tpl.env | sed -e "s|{{LETSENCRYPT_MAIL}}|$LETSENCRYPT_MAIL|g;s|{{TRANSMISSION_VHOST}}|$TRANSMISSION_VHOST|g;s|{{TRANSMISSION_LETSENCRYPT_HOST}}|$TRANSMISSION_LETSENCRYPT_HOST|g;s|{{TRANSMISSION_VIRTUAL_PORT}}|$TRANSMISSION_VIRTUAL_PORT|g" > transmission.env
-cat docker-compose.tpl.yml | sed -e "s|{{NEXTCLOUD_DATA}}|$NEXTCLOUD_DATA|g;s|{{TRANSMISSION_DATA}}|$TRANSMISSION_DATA|g;s|{{TRANSMISSION_CONFIG}}|$TRANSMISSION_CONFIG|g" > docker-compose.yml
+cat transmission.tpl.env | sed -e "s|{{LETSENCRYPT_MAIL}}|$LETSENCRYPT_MAIL|g;" > transmission.env
+cat docker-compose.tpl.yml | sed -e "s|{{TRAEFIK_CONF_FILE}}|$TRAEFIK_CONF_FILE|g;s|{{TRAEFIK_LETSENCRYPT_FILE}}|$TRAEFIK_LETSENCRYPT_FILE|g;s|{{NEXTCLOUD_VHOST}}|$NEXTCLOUD_VHOST|g;s|{{NEXTCLOUD_DATA}}|$NEXTCLOUD_DATA|g;s|{{NEXTCLOUD_CONFIG}}|$NEXTCLOUD_CONFIG|g;s|{{TRANSMISSION_VHOST}}|$TRANSMISSION_VHOST|g;s|{{TRANSMISSION_DATA}}|$TRANSMISSION_DATA|g;s|{{TRANSMISSION_CONFIG}}|$TRANSMISSION_CONFIG|g" > docker-compose.yml
+cat traefik/traefik.tpl.toml | sed -e "s|{{LETSENCRYPT_MAIL}}|$LETSENCRYPT_MAIL|g;s|{{COCKPIT_VHOST}}|$COCKPIT_VHOST|g;s|{{DOMAIN}}|$DOMAIN|g;s|{{VHOST_LIST}}|$VHOST_LIST|g" > traefik/traefik.toml
+
+
